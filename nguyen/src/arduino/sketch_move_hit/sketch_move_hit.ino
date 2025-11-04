18b10d236f3d917c8b731094c49c068f5323d5d4
take62 <144413779+take62@users.noreply.github.com>
2025-11-04 15:48:00 +0900

    Update sketch_move_hit.ino

--- nguyen/src/arduino/sketch_move_hit/sketch_move_hit.ino
+++ nguyen/src/arduino/sketch_move_hit/sketch_move_hit.ino
@@ -3,7 +3,7 @@
 #include <Servo.h>
 #include "nakano.h"
 
-#define DEBUG
+//#define DEBUG
 
 //============== SERVO: takemichi ==================//
 Servo servoA;
@@ -16,6 +16,31 @@ int angleC = 105;
 //============== MOVE: Nakano ==================//
 // 長いためヘッダーファイルに分割
 
+
+//超音波
+int sensorPin = A1;
+int ledPin1 = 13;
+int ledPin2 = 12;
+int ledPin3 = 11;
+int i = 0;
+int ave = 0;
+unsigned long now = 0;
+
+const int threshold_MAX = 500;
+const int threshold_ave = 450;
+
+const int MAX_SAMPLES = 100;      // 500msで入れたい最大サンプル数
+int buf[MAX_SAMPLES];
+int sampleCount = 0;
+
+int maxValue = 0;
+unsigned long lastMs = 0;
+
+
+//  LEDを点灯させる しきい値 
+// sensorValue がこの値(0〜1023)より大きくなったらLEDが光ります。
+const int threshold = 140; 
+
 // その他
 const int EXPECTED_LEN = 10;
 
@@ -25,8 +50,13 @@ void setup() {
   Serial.setTimeout(50);  // readStringUntilの待ち時間（ms）
 
   // takemichi servo setup
+<<<<<<< HEAD
   servoA.attach(11);   // デカサーボ CNC:Z
   servoB.attach(10);  // ミニサーボ CNC:Y
+=======
+  servoA.attach(11);   // デカサーボ
+  servoB.attach(10);  // ミニサーボ
+>>>>>>> 825ec6256baab07efc2f5504cf3513b741dd4dd1
   servoC.attach(9);  // 角度サーボ
 
   servoB.write(150);
@@ -47,6 +77,10 @@ void setup() {
   x_dir_sign = +1;
   y_dir_sign = +1;
   t_next = micros();
+
+  pinMode(ledPin1, OUTPUT);
+  pinMode(ledPin2, OUTPUT);
+  pinMode(ledPin3, OUTPUT);
 }
 void loop() {
 
@@ -136,6 +170,87 @@ void loop() {
       servoA.write(30);
       break;
     }
+
+    case 3: {
+      int v = analogRead(sensorPin);
+
+      // maxの更新
+      if (v > maxValue) {
+        maxValue = v;
+      }
+
+      if (v > 420 && sampleCount < MAX_SAMPLES) {
+        buf[sampleCount] = v;
+        sampleCount++;
+      }
+
+      now = millis();
+      if (now - lastMs >= 200) {
+        //int ave = 0;
+        if (sampleCount > 0) {
+          // 雑に平均を出す
+          long sum = 0;
+          for (int k = 0; k < sampleCount; k++) {
+            sum += buf[k];
+          }
+          int roughAve = sum / sampleCount;
+
+          // 平均から離れてるのを除外し再平均
+          sum = 0;
+          int validCount = 0;
+          const int ALLOW_DIFF = 50;   // これより離れてたら外れとみなす（要調整）
+          for (int k = 0; k < sampleCount; k++) {
+            if (abs(buf[k] - roughAve) <= ALLOW_DIFF) {
+              sum += buf[k];
+              validCount++;
+            }
+          }
+
+          if (validCount > 0) {
+            ave = sum / validCount;
+          } else {
+            ave = 0;   // 全部外れた場合
+          }
+        }
+
+      
+      // 出力
+      //Serial.print("MAX:"); Serial.print(maxValue);
+      if(maxValue>threshold_MAX){
+      //Serial.print("*");
+      }
+      //Serial.print(" AVE:"); Serial.print(ave);
+      if(ave>threshold_ave){
+      //Serial.print("*");
+      }
+      //Serial.print(" N:");   Serial.print(sampleCount);  
+
+      //認識
+      if (maxValue > threshold_MAX && ave > threshold_ave) {
+        //Serial.print("  !");
+        digitalWrite(ledPin1, HIGH);
+        //i = i+1;
+      }else{
+        digitalWrite(ledPin1, LOW);
+        digitalWrite(ledPin2, LOW);
+        x_cmd = ROT_CCW_X_SIGN * 0.10f;
+        y_cmd = ROT_CCW_Y_SIGN * (-0.10f); 
+        driveVelocity(x_cmd, y_cmd, dt);
+       // i=0;
+      }
+
+
+      //Serial.println();
+
+      // 片付け
+      maxValue = 0;
+      sampleCount = 0;
+      lastMs = now;
+      }
+      
+      //delay(5); 
+      break;         
+    }  
 
     default: {
       // mode_valが1でも2でもない → 何もしない（停止）
