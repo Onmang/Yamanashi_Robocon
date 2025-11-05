70e557ba1743cd14c4ec2da4837ee8716f7cad90
Nguyen <125030709+Onmang@users.noreply.github.com>
2025-11-05 19:08:16 +0900

    recog3

--- nguyen/src/recognition_ver3.py
+++ nguyen/src/recognition_ver3.py
@@ -20,24 +20,29 @@ from common_function import (
     compute_center_distance,
     change_camera,
     get_rgbd_images,
+    preprocess_depth_and_hsv,
+    circularity_and_hough,
+    evaluate_circle_detection,
+    smooth_angle_distance,
     PARAM_PATH_DIS_D435I,
     PARAM_PATH_HSV,
     PARAM_PATH_DIS_D405,
+    PARAM_PATH_DIS_GREEN,
     PARAM_HOUGH_D405,
     PARAM_HOUGH_D435I,
-    PARAM_FILTER
+    PARAM_FILTER,
 )
 
 # debug
 DEBUG = True  # True: デバッグモードON, False: デバッグモードOFF
 CIRC_MIN = 0.80
 AREA_MIN = 100  # 小ノイズ除去
-CHANGE_CAMERA_THRE_D435I = 350 # mm
-CHANGE_CAMERA_THRE_D405 = 550 # mm
-STOP_DIS_D405 = 100 # mm
+CHANGE_CAMERA_THRE_D435I = 350  # mm
+CHANGE_CAMERA_THRE_D405 = 550  # mm
+STOP_DIS_D405 = 100  # mm
 
 # arduino シリアル通信設定
-ARDUINO = False 
+ARDUINO = False
 if ARDUINO:
     global ser
 
@@ -55,6 +60,7 @@ if ARDUINO:
     if DEBUG:
         print("Serial Port was opened:", serial_port)
 
+
 def main():
     try:
         # --------------------------------------------
@@ -63,41 +69,45 @@ def main():
         # hsvパラメータ読み込み
         flag_lo, flag_hi = load_hsv_from_json(PARAM_PATH_HSV[3])
         white_lo, white_hi = load_hsv_from_json(PARAM_PATH_HSV[8])
-        
+
         # ガウシアンフィルター
         gaus_k, sigmaX = load_filter_params_from_json(PARAM_FILTER)
+
+        # 距離フィルタのデフォルト値
+        green_dist_min_cm, green_dist_max_cm = load_filter_distance_from_json(
+            PARAM_PATH_DIS_GREEN
+        )
 
         # --------------------------------------------
         # カメラ初期化
         # --------------------------------------------
         # 解像度とFPS
         W, H, FPS = 640, 480, 15
-        
-        
-    # RealSense D435i カメラ初期化
-    cam_d435i = init_realsense_camera(
-        name="d435i",
-        serial="949122070535",  # 実機のシリアル
-        width=W,
-        height=H,
-        fps=FPS,
-        extrinsic_guess={
-            "tx": -(32.5 * 0.001),
-            "ty": -50 * 0.001,
-            "tz": 200 * 0.001,
-            "rx_deg": -90, 
-            "ry_deg": 0,
-            "rz_deg": 0,
-        },
-        dis_param_path=PARAM_PATH_DIS_D435I,
-        hough_param_path=PARAM_HOUGH_D435I,
-        ball_hsv_param_path=PARAM_PATH_HSV[2],
-    )
 
-    # RealSense D405 カメラ初期化
-    # d405はcam3d
-    cam_d405 = init_realsense_camera(
-            name="d405",        
+        # RealSense D435i カメラ初期化
+        cam_d435i = init_realsense_camera(
+            name="d435i",
+            serial="949122070535",  # 実機のシリアル
+            width=W,
+            height=H,
+            fps=FPS,
+            extrinsic_guess={
+                "tx": -(32.5 * 0.001),
+                "ty": -50 * 0.001,
+                "tz": 200 * 0.001,
+                "rx_deg": -90,
+                "ry_deg": 0,
+                "rz_deg": 0,
+            },
+            dis_param_path=PARAM_PATH_DIS_D435I,
+            hough_param_path=PARAM_HOUGH_D435I,
+            ball_hsv_param_path=PARAM_PATH_HSV[2],
+        )
+
+        # RealSense D405 カメラ初期化
+        # d405はcam3d
+        cam_d405 = init_realsense_camera(
+            name="d405",
             serial="218622274519",  # 実機のシリアル
             width=W,
             height=H,
@@ -113,43 +123,72 @@ def main():
             dis_param_path=PARAM_PATH_DIS_D405,
             hough_param_path=PARAM_HOUGH_D405,
             ball_hsv_param_path=PARAM_PATH_HSV[9],
-    )
-    
-    # init activate cam
-    activate_cam = cam_d435i
-    
-    # --------------------------------------------
-    # windown関係
-    # --------------------------------------------
-    if DEBUG:
-        # 作成
-        cv2.namedWindow("Input", cv2.WINDOW_NORMAL)
-        cv2.namedWindow("HSV Mask Morph", cv2.WINDOW_NORMAL)
-        cv2.namedWindow("Result", cv2.WINDOW_NORMAL)
+        )
 
-        # サイズ変更
-        w_re = 300
-        h_re = 250
-        cv2.resizeWindow("Input", w_re, h_re)
-        cv2.resizeWindow("HSV Mask Morph", w_re, h_re)
-        cv2.resizeWindow("Result", w_re, h_re)
+        # init activate cam
+        activate_cam = cam_d435i
 
-    # --------------------------------------------
-    # メインループ
-    # --------------------------------------------
-    print("[DEBUG] main loop....")
-    mode = 0    # 送信フラグ
-    EMA_ALPHA = 0.30  # 0.1～0.5 で調整（大きいほど追従が速い／ノイズに弱い）
-    MISS_LIMIT = 5  # 短期見失いの許容量（フレーム数）
-    prev_angle = 0  # 直近の平滑化角度[deg]
-    prev_dist = 0  # 直近の平滑化距離[mm]
-    miss_count = 0  # 見失いカウンタ
-    rasp_mode = 100  
-    hitted_flag = False
-    hit_ready = False
-    Hit_n = 1
-        
-        
+        # --------------------------------------------
+        # windown関係
+        # --------------------------------------------
+        if DEBUG:
+            # 作成
+            cv2.namedWindow("Input", cv2.WINDOW_NORMAL)
+            cv2.namedWindow("HSV Mask Morph", cv2.WINDOW_NORMAL)
+            cv2.namedWindow("Result", cv2.WINDOW_NORMAL)
+
+            # サイズ変更
+            w_re = 300
+            h_re = 250
+            cv2.resizeWindow("Input", w_re, h_re)
+            cv2.resizeWindow("HSV Mask Morph", w_re, h_re)
+            cv2.resizeWindow("Result", w_re, h_re)
+
+        # --------------------------------------------
+        # メインループ
+        # --------------------------------------------
+        print("[DEBUG] main loop....")
+        EMA_ALPHA = 0.30  # 0.1～0.5 で調整（大きいほど追従が速い／ノイズに弱い）
+        MISS_LIMIT = 5  # 短期見失いの許容量（フレーム数）
+        prev_angle = 0  # 直近の平滑化角度[deg]
+        prev_dist = 0  # 直近の平滑化距離[mm]
+        miss_count = 0  # 見失いカウンタ
+        rasp_mode = 100
+        hitted_flag = False
+        hit_ready = False
+        Hit_n = 1
+
+        # main loop
+        while True:
+            # ここにメインループの処理を記述
+            state = "LOST"
+            sent = False
+            mode = 0
+
+            # 画像取得
+            color_image, depth_image = get_rgbd_images(activate_cam)
+
+            # 場合分け
+            match rasp_mode:
+                ### ボール探索モード ###
+                case 100:  
+                    # 前処理の2値化
+                    mask_morph, vis = preprocess_depth_and_hsv(color_image, depth_image, 
+                             activate_cam, 
+                             dist_min_cm=None, dist_max_cm=None,
+                             gaus_k=gaus_k, sigmaX=sigmaX,
+                             hsv_lo=activate_cam.ball_lo, hsv_hi=activate_cam.ball_hi)
+
+                    # 円検出
+                    circles = circularity_and_hough(mask_morph, activate_cam, area_min=AREA_MIN, circ_min=CIRC_MIN)
+                    
+                    # 円検出の評価
+                    circl_res, x, y, r, cam3d, rob3d = evaluate_circle_detection(circles, depth_image, activate_cam)
+                    
+                    if circl_res:
+                    
+                    
+
     except KeyboardInterrupt:
         print("[FINISHED].")
     finally:
@@ -161,6 +200,5 @@ def main():
             cv2.destroyAllWindows()
 
 
-    
 if __name__ == "__main__":
-    main()    # arduino シリアル通信設定
+    main()  # arduino シリアル通信設定
